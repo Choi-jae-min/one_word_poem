4528c268579d6c32ec455baf769a996ed227b40b
jasmin <a01095567997@gmail.com>
2026-08-05 02:56:01 +0900

    feat : 제목, 본문 텍스트 필스 생성

--- lib/components/latter_box.dart
+++ lib/components/latter_box.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:one_word_poem/components/cards/topic_card.dart';
+import 'package:one_word_poem/components/textFields/default_text_field.dart';
 import 'package:one_word_poem/components/textFields/text_area.dart';
 
 import 'buttons/color_button.dart';
@@ -14,6 +15,18 @@ class LatterBox extends StatefulWidget {
 }
 
 class _LatterBoxState extends State<LatterBox> {
+  final _titleController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
+  }
+
+  void submit() {
+    final title = _titleController.text;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -36,63 +49,69 @@ class _LatterBoxState extends State<LatterBox> {
             fit: BoxFit.fill
           ),
           Padding(
-            padding: const EdgeInsets.fromLTRB(20, 20, 20, 38),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
+            padding: const EdgeInsets.fromLTRB(20, 30, 20, 38),
+            child: ListView(
+              physics: const ClampingScrollPhysics(),
               children: [
-                SizedBox.fromSize(size: Size(10, 30),),
-                const Text(
-                  '오늘의 마음을 짧은 시로 남겨보세요',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-                Container(
-                  key: Key("dot_line"),
-                  padding: EdgeInsets.fromLTRB(30, 0,10, 0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Expanded(
-                        child: CustomPaint(
-                          painter: DashedLinePainter(),
-                        ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox.fromSize(size: Size(10, 30),),
+                    const Text(
+                      '오늘의 마음을 짧은 시로 남겨보세요',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
                       ),
-                      const SizedBox(width: 4),
-                      Image.asset(
-                          'assets/articles/botanical_branch.png',
-                          height: 25,
-                          fit: BoxFit.contain,
+                    ),
+                    Container(
+                      key: Key("dot_line"),
+                      padding: EdgeInsets.fromLTRB(30, 0,10, 0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Expanded(
+                            child: CustomPaint(
+                              painter: DashedLinePainter(),
+                            ),
+                          ),
+                          const SizedBox(width: 4),
+                          Image.asset(
+                              'assets/articles/botanical_branch.png',
+                              height: 25,
+                              fit: BoxFit.contain,
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 30),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text('오늘의 주제',style: TextStyle(
+                          fontSize: 12
+                        ),),
+                      ),
+                    ),
+                    TopicCard(topic: "비 오늘날 하늘"),
+                    DefaultTextField(textTitle: "시 제목",hintText: "제목을 적어주세요", maxLength: 20, controller: _titleController),
+                    TextArea(textTitle:"시 쓰기" ,hintText: "여기에 마음을 적어보세요", maxLength: 120),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(10,6,10,10),
+                      child: Row(
+                        children: [
+                          DefaultButton(title: "예시 보기" , onPressed: () => {
+                            print("예시 보기")
+                          }),
+                          const SizedBox(width: 12),
+                          ColorButton(title: "저장 하기", themeColor: const Color(0xFFD86632), onPressed: () => {
+                            print("저장하기")
+                          }),
+                        ],
+                      ),
+                    )
+                  ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 30),
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text('오늘의 주제',style: TextStyle(
-                      fontSize: 12
-                    ),),
-                  ),
-                ),
-                TopicCard(topic: "비 오늘날 하늘"),
-                TextArea(hintText: "여기에 마음을 적어보세요", maxLength: 20),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(10,6,10,10),
-                  child: Row(
-                    children: [
-                      DefaultButton(title: "예시 보기" , onPressed: () => {
-                        print("예시 보기")
-                      }),
-                      const SizedBox(width: 12),
-                      ColorButton(title: "저장 하기", themeColor: const Color(0xFFD86632), onPressed: () => {
-                        print("저장하기")
-                      }),
-                    ],
-                  ),
-                )
               ],
             ),
           ),
---
+++ lib/components/textFields/default_text_field.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class DefaultTextField extends StatefulWidget {
+  final String hintText;
+  final String textTitle;
+  final int maxLength;
+  final TextEditingController controller;
+
+  const DefaultTextField({super.key,
+    required this.textTitle,
+    required this.hintText,
+    required this.maxLength,
+    required this.controller,
+  });
+
+  @override
+  State<DefaultTextField> createState() => _DefaultTextFieldState();
+}
+
+class _DefaultTextFieldState extends State<DefaultTextField> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsetsGeometry.symmetric(horizontal: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 5),
+          Text(widget.textTitle),
+          ValueListenableBuilder<TextEditingValue>(
+            valueListenable: widget.controller,
+            builder: (context,value,child){
+              return TextField(
+                  controller: widget.controller,
+                  cursorHeight: 14,
+                  maxLength: widget.maxLength,
+                  maxLines: 1,
+                  textInputAction: TextInputAction.done,
+                  inputFormatters: [
+                    FilteringTextInputFormatter.deny(RegExp(r'[\r\n]')),
+                    LengthLimitingTextInputFormatter(
+                        widget.maxLength,
+                      maxLengthEnforcement: MaxLengthEnforcement.enforced
+                    ),
+                  ],
+                  textAlignVertical: TextAlignVertical.top,
+                  style: const TextStyle(
+                    fontSize: 14,
+                    height: 2,
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xFF493326),
+                  ),
+                  decoration: InputDecoration(
+                    counterText: '',
+                    hintText: widget.hintText,
+                    hintStyle: const TextStyle(
+                      fontSize: 14,
+                      color: Color(0xFF9B948C),
+                    ),
+
+                    suffixIcon: IgnorePointer(
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 20),
+                        child: Center(
+                          widthFactor: 1,
+                          child: Text(
+                            '${value.text.characters.length} / ${widget.maxLength}',
+                            style: const TextStyle(
+                              fontSize: 14,
+                              color: Color(0xFF6E6257),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    suffixIconConstraints: const BoxConstraints(
+                      minWidth: 0,
+                      minHeight: 0,
+                    ),
+
+                    contentPadding: const EdgeInsets.symmetric(
+                      horizontal: 15,
+                    ),
+
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(
+                        color: Color(0xFFD8CFC3),
+                        width: 1.5,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(
+                        color: Color(0xFF9B948C),
+                        width: 1.5,
+                      ),
+                    ),
+                  )
+              );
+            },
+          ),
+        ],
+      )
+    );
+  }
+}
